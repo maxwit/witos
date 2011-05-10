a5885c79c2d9f724f045b7ea91ce4599688c43a0
Annie Hou <annie.hou@maxwit.com>
2011-05-10 15:15:51 +0800

    add include block.h
    Signed-off-by: Annie Hou <annie.hou@maxwit.com>

---
+++ include/block.h
@@ -0,0 +1,41 @@
+#pragma once
+
+typedef enum
+{
+	PT_NONE,
+	PT_BAD,
+	PT_FREE,
+	PT_BL_GTH,
+	PT_BL_GBH,
+	PT_BL_GCONF,
+	PT_BL_UBOOT,
+	PT_OS_LINUX,
+	PT_OS_WINCE,
+	PT_FS_RAMDISK,
+	PT_FS_CRAMFS,
+	PT_FS_BEGIN = PT_FS_CRAMFS,
+	PT_FS_JFFS2,
+	PT_FS_YAFFS,
+	PT_FS_YAFFS2,
+	PT_FS_UBIFS,
+	PT_FS_END = PT_FS_UBIFS
+} PART_TYPE;
+
+#define PART_NAME_LEN 32
+
+struct part_attr
+{
+	PART_TYPE part_type;
+	u32   part_base;
+	u32   part_size;
+	char  part_name[PART_NAME_LEN];
+};
+
+struct block_device
+{
+	char part_name[PART_NAME_LEN];
+	u32  part_base;
+	u32  part_size;
+};
+
+int block_device_register(struct block_device *blk_dev);
