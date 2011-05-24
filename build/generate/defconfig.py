dac74430bc7a160c8986e6ba23f9460b722de49d
Annie Hou <annie.hou@maxwit.com>
2011-05-24 14:36:56 +0800

    xml
    Signed-off-by: Annie Hou <annie.hou@maxwit.com>

--- build/generate/defconfig.py
+++ build/generate/defconfig.py
@@ -10,22 +10,35 @@
 import os,sys,re
 from xml.etree import ElementTree
 
-default_arch = 'ARM'
-
-def traverse(node):
+def traverse(node, prefix=None):
 	if node.tag == 'choice':
 		lst = node.getchildren()
+
+		# check node ... from .config
+		if node.attrib.has_key('default'):
+			default_node = node.attrib['default'].upper()
+		else:
+			default_node = lst[0].attrib['name'].upper()
+
 		for n in lst:
-			if n.attrib['string'].upper() == default_arch.upper():
-				traverse(n)
+			if n.attrib['name'].upper() == default_node:
+				traverse(n, node.attrib['name'].upper())
+	elif node.tag == 'select':
+		l2 = node.text.split(' ')
+		for s in l2:
+			print 'CONFIG_' + s + ' = y'
 	else:
 		if node.tag == 'config':
 			if node.attrib.has_key('name'):
 				name = node.attrib['name'].upper()
+				if prefix <> None:
+					name = prefix + '_' + name
 				if node.attrib.has_key('string'):
 					print 'CONFIG_%s = "%s"' % (name, node.attrib['string'])
 				elif node.attrib.has_key('bool'):
 					print 'CONFIG_%s = %s' % (name, node.attrib['bool'])
+				elif node.attrib.has_key('int'):
+					print 'CONFIG_%s = %s' % (name, node.attrib['int'])
 	
 		lst = node.getchildren()
 		for n in lst:
