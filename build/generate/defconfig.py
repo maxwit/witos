5b6507cf74cfd42385f57d99a9036707d5f951f9
Conke Hu <conke.hu@maxwit.com>
2011-05-24 18:52:51 +0800

    config: step forward:)
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build/generate/defconfig.py
+++ build/generate/defconfig.py
@@ -10,11 +10,13 @@
 import os,sys,re
 from xml.etree import ElementTree
 
-def traverse(node, prefix=None):
+config = {}
+
+def traverse(node):
 	if node.tag == 'choice':
 		lst = node.getchildren()
 
-		# check node ... from .config
+		# fixme: check node ... from .config
 		if node.attrib.has_key('default'):
 			default_node = node.attrib['default'].upper()
 		else:
@@ -22,27 +24,37 @@ def traverse(node, prefix=None):
 
 		for n in lst:
 			if n.attrib['name'].upper() == default_node:
-				traverse(n, node.attrib['name'].upper())
+				traverse(n)
+			# fixme: check matched or not
 	elif node.tag == 'select':
 		l2 = node.text.split(' ')
 		for s in l2:
 			print 'CONFIG_' + s + ' = y'
 	else:
+		dfs = True
 		if node.tag == 'config':
 			if node.attrib.has_key('name'):
 				name = node.attrib['name'].upper()
-				if prefix <> None:
-					name = prefix + '_' + name
+
 				if node.attrib.has_key('string'):
-					print 'CONFIG_%s = "%s"' % (name, node.attrib['string'])
+					key = 'CONFIG_' + name
+					if config.has_key(key) == False:
+						config[key] = '"' + node.attrib['string'] + '"'
 				elif node.attrib.has_key('bool'):
-					print 'CONFIG_%s = %s' % (name, node.attrib['bool'])
+					if node.attrib['bool'] == "y":
+						print 'CONFIG_%s = %s' % (name, node.attrib['bool'])
+					else:
+						dfs = False
 				elif node.attrib.has_key('int'):
 					print 'CONFIG_%s = %s' % (name, node.attrib['int'])
+				else:
+					print 'Invalid node!'
+					print node
 	
-		lst = node.getchildren()
-		for n in lst:
-			traverse(n)
+		if dfs == True:
+			lst = node.getchildren()
+			for n in lst:
+				traverse(n)
 
 def parse_tree(fn):
 	try:
@@ -60,5 +72,19 @@ if __name__ == "__main__":
 	if os.getenv('USER') == 'root':
 		print 'cannot run as root!'
 		exit(1)
+
+	try:
+		fcfg = open(".config")
+	except:
+		print 'fail to open .config'
+		parse_tree("build/configs/configs.xml")
+		exit(0)
+
+	for line in fcfg:
+		if re.match(r'^CONFIG_', line) <> None:
+			elem = line.replace('\n', '').split('=') #\s+=\s+
+			config[elem[0]] = elem[1]
+
+	fcfg.close()
 
 	parse_tree("build/configs/configs.xml")
