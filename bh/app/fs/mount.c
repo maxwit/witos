c722d18d6672cd62fa1e4bc218252e580cc38c4c
Conke Hu <conke.hu@maxwit.com>
2011-09-02 17:29:10 +0800

    add expception handle for mount app
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- bh/app/fs/mount.c
+++ bh/app/fs/mount.c
@@ -5,14 +5,21 @@
 int main(int argc, char *argv[])
 {
 	int ret;
+	const char *bdev_name;
 
 	if (argc != 2)
 	{
-		printf("Usage:\n\t%s device\n", argv[1]);
+		printf("Usage:\n\t%s device\n", argv[0]);
 		return -EINVAL;
 	}
 
-	ret = mount("vfat", 0, argv[1], NULL);
+	bdev_name = argv[1];
+
+	ret = mount("vfat", 0, bdev_name, NULL);
+	if (ret < 0)
+	{
+		printf("fail to mount %s (ret = %d)\n", bdev_name, ret);
+	}
 
 	return ret;
 }
