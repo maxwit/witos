2f60e5ea65dd839f4742b731cfcb74be7dadab9e
Conke Hu <conke.hu@maxwit.com>
2011-09-26 19:18:24 +0800

    fix foo driver
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- bh/fs/demo/block.c
+++ bh/fs/demo/block.c
@@ -21,8 +21,6 @@ struct block_device *get_bdev_by_name(const char *name)
 
 int block_device_register(struct block_device *bdev)
 {
-	// printf("%s(): registering %s\n", __func__, bdev->dev.name);
-
 	list_add_tail(&bdev->bdev_node, &g_bdev_list);
 
 	return 0;
--- bh/fs/demo/fat_demo.c
+++ bh/fs/demo/fat_demo.c
@@ -2,14 +2,20 @@
 
 int block_device_init();
 int disk_drive_init();
-int foo_card_register();
+int foo_drv_init();
 
-int main()
+int main(int argc, char *argv[])
 {
+	const char *fn;
+
+	if (2 == argc)
+		fn = argv[1];
+	else
+		fn = "hd.img";
 
 	block_device_init();
 	disk_drive_init();
-	foo_card_register();
+	foo_drv_init(fn);
 
 	return 0;
 }
