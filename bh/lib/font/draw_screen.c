cb27049473742f7e8d491904199f5408fa0e21cf
rouchel <rouchel.yan@maxwit.com>
2011-10-14 15:39:32 +0800

    font support
    	If you want to support a new font, you can just put your new_font.c
    	into the dir bh/lib/font, and rename the file like font_8*16.c, it
    	well be very easy!
    
    Signed-off-by: rouchel <rouchel.yan@maxwit.com>

---
+++ bh/lib/font/draw_screen.c
@@ -0,0 +1,74 @@
+#include <font/font.h>
+#include <graphic/display.h>
+
+static int is_dot(int x, int y, char c, struct font_descript *font)
+{
+	int w, h;
+	u8 *data;
+
+	w = (font->width + 7) / 8;
+	h = c * font->height + y;
+
+	data = (u8 *)font->data + w * h;
+	data += x / 8;
+	x = x % 8;
+
+	return (*data >> (7 - x)) & 0x01;
+}
+
+int draw_char(struct display *disp, int x, int y, struct font_descript *font, u32 color, char c)
+{
+	int width  = disp->video_mode->width;
+	u16 *buff;
+	int i, j;
+
+	buff = (u16 *)disp->video_mem_va;
+
+	for (i = 0; i < font->height; i++)
+	{
+		for (j = 0; j < font->width; j++)
+		{
+			// fixme
+			if (is_dot(j, i, c, font))
+				buff[(i + y) * width + x + j] = color;
+		}
+	}
+
+	return 0;
+}
+
+int draw_string(struct display *disp, int x, int y, struct font_descript *font, u32 color, const char * str)
+{
+	while (*str)
+	{
+		draw_char(disp, x, y, font, color, *str);
+		str++;
+		x += font->width;
+	}
+
+	return 0;
+}
+
+int set_screen(u32 pixel)
+{
+	u16 *buff;
+	int height, width;
+	int i, j;
+	struct display *disp;
+
+	disp = get_system_display();
+	buff = (u16 *)disp->video_mem_va;
+	height = disp->video_mode->height;
+	width  = disp->video_mode->width;
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			// fixme
+			buff[i * width + j] = pixel;
+		}
+	}
+
+	return 0;
+}
