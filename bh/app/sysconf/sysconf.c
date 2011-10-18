bae760d2a29ba570edac873ce8ee31b272a9118f
rouchel <rouchel.yan@maxwit.com>
2011-10-18 12:20:01 +0800

    fix sysconf usage
    Signed-off-by: rouchel <rouchel.yan@maxwit.com>

--- bh/app/sysconf/sysconf.c
+++ bh/app/sysconf/sysconf.c
@@ -4,8 +4,13 @@
 
 static void usage(void)
 {
-	printf("Usage:\n"
-		"\t-r <all|net|boot>: reset system configuration.\n");
+	printf("Usage: sysconfig [OPTION [VAL]]\n"
+		"Operate system configuration information!\n"
+		"\nOPTION:\n"
+		"\t-l : List system configuration information!"
+		"\t-r [net|boot]: reset system configuration(default all).\n"
+		"\t-s : save the current sysconfig to the storage.\n"
+		"\t-h : this help message.\n");
 }
 
 int main(int argc, char *argv[])
@@ -13,6 +18,8 @@ int main(int argc, char *argv[])
 	int ret, opt;
 	BOOL reset_all = FALSE;
 	char *arg;
+
+	usage();
 
 	while ((opt = getopt(argc, argv, "r:h", &arg)) != -1)
 	{
