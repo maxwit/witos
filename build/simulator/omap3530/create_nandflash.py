d51635de54e00a9fc07a3662e83179e97d308f7e
Rouchel Yan <rouchel.yan@maxwit.com>
2011-10-24 22:49:45 +0800

    create_nandflash.py: rewrite in OOB and fix arguments
    Signed-off-by: Liang Chen <liang.chen@maxwit.com>
    Signed-off-by: Martin Liang <martin.liang@maxwit.com>

--- build/simulator/omap3530/create_nandflash.py
+++ build/simulator/omap3530/create_nandflash.py
@@ -4,6 +4,7 @@ import sys
 import os
 import getopt
 
+# switch class
 class switch(object):
 	def __init__ (self, value):
 		self.value=value
@@ -22,59 +23,60 @@ class switch(object):
 		else:
 			return False
 
-# default Nand flash partitions
-#0    0x00000000 - 0x00080000  512K      g-bios-th  "g-bios"
-#1    0x00080000 - 0x00280000  2M        g-bios-bh  "g-bios"
-#2    0x00280000 - 0x002A0000  128K      sysconfig  "g-bios"
-#3    0x002A0000 - 0x005A0000  3M        linux      "linux"
-#4    0x005A0000 - 0x008A0000  3M        ramdisk    "ramdisk"
-#5    0x008A0000 - 0x048A0000  64M       jffs2      "rootfs"
-#6    0x048A0000 - 0x088A0000  64M       yaffs2     "data_1"
-#7    0x088A0000 - 0x10000000  119M384K  ubifs      "data_2"
+# mkae image class
+class make_image:
+	def __init__ (self):
+		self.flash_image_name = ""
 
-# make image
-def create(flash_block_size, flash_oob_size, flash_image_name, flash_page_size, flash_image_pages):
-	data = "\xff"
-	count = 0
+		# default size
+		self.flash_page_size   = 2048
+		self.flash_oob_size    = 64
+		self.flash_block_size  = 64
+		self.flash_image_pages = 131072
 
-	data = data * (flash_page_size + flash_oob_size) # A page is 2K bytes of data + 64bytes OOB
-	data_block = data * flash_block_size # A block is "flash_block_size" page
+	# make image
+	def create(self):
+		data = "\xff"
+		count = 0
 
-	fd = open(flash_image_name, "w+");
+		data = data * (self.flash_page_size + self.flash_oob_size) # A page is 2K bytes of data + 64bytes OOB
+		data_block = data * self.flash_block_size # A block is "flash_block_size" page
 
-	total = len(data) * flash_image_pages / len(data_block)
-	while 1:
-		fd.write(data_block)
-		count = count + 1
-		if count == total:
-			break
+		fd = open(self.flash_image_name, "w+")
 
-	fd.close()
+		total = len(data) * self.flash_image_pages / len(data_block)
+		while 1:
+			fd.write(data_block)
+			count = count + 1
+			if count == total:
+				break
 
-def put_no_oob(image_name, image_page_offset, flash_page_size):
-	size = os.path.getsize(image_name)
-	image_pages = size / flash_page_size
+		fd.close()
 
-	if (size % flash_page_size) != 0:
-		image_pages = image_pages + 1
+	def put_no_oob(self, image_name, image_page_offset):
+		data = "\xff"
+		size = os.path.getsize(image_name)
+		image_pages = size / self.flash_page_size
 
-	i = 0
-	while i < image_pages:
-		out_offset = image_page_offset + i
-		in_offset = i
+		if (size % self.flash_page_size) != 0:
+			image_pages = image_pages + 1
 
-		cmd = "dd if=" + image_name + " of=" + flash_image_name + " conv=notrunc count=1 obs=" + str(flash_page_size+flash_oob_size) + " ibs=" + str(flash_page_size) + " seek=" + str(out_offset) + " skip=" + str(in_offset)
+		data = data * self.flash_oob_size
+		fd_image = open(image_name, "rb")
+		fd_flash = open(self.flash_image_name, "rw+")
+		fd_flash.seek((image_page_offset * (self.flash_page_size + self.flash_oob_size)))
+		i = 0
+		while i < image_pages:
+			read_image = fd_image.read(self.flash_page_size)
+			read_image = read_image + data
+			fd_flash.write(read_image)
+			i = i + 1
 
-		os.system(cmd) #fixme
-		i = i + 1
+		fd_image.close()
+		fd_flash.close()
 
 def usage():
-	print 'Usage:	' + sys.argv[0] + ' <-i image> <-o destimage>'
-	print '	[-p page-size] [-b block-size] [-f oob-size]\n'
-	print '-b	Block size (default: 64 page)'
-	print '-f	OOB size (default: 64)'
-	print '-i	Input image FILE (g-bios-th.bin, g-bios-bh.bin, zImage, rootfs)'
-	print '	rootfs:	yaffs2, jffs2, ubifs'
+	print 'Usage:	' + sys.argv[0] + ' <bl1> <bl2> <-o destimage> [-p page-size]\n'
 	print '-o	Output to FILE'
 	print '-p	Page size (default: 2048)'
 	print '-h	display this help text\n'
@@ -83,161 +85,115 @@ def usage():
 	print '512		32		16'
 	print '1024		64		32'
 	print '2048		64		64'
-	print '4069		64		128\n'
+	print '4096		64		128\n'
 	print 'Examples:'
-	print '	' + sys.argv[0] + ' -i g-bios-th.bin -o flash.img'
-	print '	' + sys.argv[0] + ' -i zImage -o flash.img'
-	print '	' + sys.argv[0] + ' -i ubifs -o flash.img'
-	print '	' + sys.argv[0] + ' -i g-bios-th -o flash.img -p 2048'
-	print '	' + sys.argv[0] + ' -i g-bios-th -o flash.img -p 2048 -b 64 -f 64'
+	print '	' + sys.argv[0] + ' th.bin bh.bin -o flash.img'
+	print '	' + sys.argv[0] + ' th.bin bh.bin -o flash.img -p 2048'
 
 if __name__ == "__main__":
 	argv_len = len(sys.argv)
-	if argv_len < 5:
+	if argv_len < 6:
 		usage()
 		sys.exit()
 
-	# default size
-	flash_page_size	  = 2048
-	flash_oob_size    = 64
-	flash_block_size  = 64
-	flash_image_pages = 131072
-	oob_size   = 64
-	block_size = 64
+	page_flags = 0
 
 	# part size
 	th_part_size = 0
 	bh_part_size = 524288
+	cfg_part_size = 2621440
 	ker_part_size = 2752512
 	jf2_part_size = 9043968
 	yaf_part_size = 37184 * 2048
 	ubi_part_size = 69952 * 2048
 
-	oob_flags   = 0
-	block_flags = 0
-	page_flags  = 0
+	mk = make_image()
+
+	bl1 = sys.argv[1]
+	bl2 = sys.argv[2]
+	bl3 = sys.argv[3]
 
 	try:
-		opts, srgs = getopt.getopt(sys.argv[1:], "b:f:i:o:p:h")
+		opts, srgs = getopt.getopt(sys.argv[1:], "o:p:h")
 	except getopt.GetoptError:
 		usage()
 		sys.exit()
 
-	for opt, arg in opts:
-		if opt == '-b':
-			flash_block_size = int(arg)
-			block_flags = 1
-
-		if opt == '-f':
-			flash_oob_size = int(arg)
-			oob_flags = 1
-
-		if opt == '-i':
-			image = arg
-
+	for opt in srgs:
 		if opt == '-o':
-			flash_image_name = arg
+			mk.flash_image_name = srgs[(srgs.index(opt) + 1)]
 
 		if opt == '-p':
-			flash_page_size = int(arg)
+			mk.flash_page_size = int(srgs[(srgs.index(opt) + 1)])
 			page_flags = 1
 
 		if opt == '-h':
 			usage()
 			sys.exit()
 
-	if os.path.exists(flash_image_name):
-		print flash_image_name, "is exists"
-	else:
-		if page_flags == 1:
-			for case in switch(flash_page_size):
-				if case(512):
-					block_size = 32
-					oob_size = 16
-					break
+	if os.path.exists(bl1) == False:
+		print bl1, "is not exists"
+		usage()
+		sys.exit()
 
-				if case(1024):
-					block_size = 64
-					oob_size = 32
-					break
+	if os.path.exists(bl2) == False:
+		print bl2, "is not exists"
+		usage()
+		sys.exit()
 
-				if case(2048):
-					block_size = 64
-					oob_size = 64
-					break
+	if os.path.exists(bl3) == False:
+		print bl3, "is not exists"
+		usage()
+		sys.exit()
 
-				if case(4096):
-					block_size = 64
-					oob_size = 128
-					break
+	if page_flags == 1:
+		for case in switch(mk.flash_page_size):
+			if case(512):
+				mk.flash_block_size = 32
+				mk.flash_oob_size = 16
+				break
 
-		if block_flags == 1:
-			if flash_block_size != block_size:
-				usage()
-				sys.exit()
-		else:
-			flash_block_size = block_size
+			if case(1024):
+				mk.flash_block_size = 64
+				mk.flash_oob_size = 32
+				break
 
-		if oob_size == 1:
-			if flash_oob_size != oob_size:
-				usage()
-				sys.exit()
-		else:
-			flash_oob_size = oob_size
+			if case(2048):
+				mk.flash_block_size = 64
+				mk.flash_oob_size = 64
+				break
 
-		if block_flags == 1 & oob_flags == 1:
-			if page_flags != 1:
-				usage()
-				sys.exit()
+			if case(4096):
+				mk.flash_block_size = 64
+				mk.flash_oob_size = 128
+				break
 
-		print "page_size : " + str(flash_page_size)
-		print "block_size: " + str(flash_block_size)
-		print "oob_size  : " + str(flash_oob_size)
+	print "image name : " + mk.flash_image_name
+	print "page_size : " + str(mk.flash_page_size)
+	print "block_size: " + str(mk.flash_block_size)
+	print "oob_size  : " + str(mk.flash_oob_size)
+
+	if os.path.exists(mk.flash_image_name) == False:
 		print "create image ..."
-		create(flash_block_size, flash_oob_size, flash_image_name, flash_page_size, flash_image_pages)
+		mk.create()  # make image
+	else:
+		print mk.flash_image_name, "is exists"
+		ret = raw_input("Remaster (y or n): ")
+		if ret == "y":
+			print "create image ..."
+			mk.create()  # make image
 
-	for case in switch(image):
-		if case("g-bios-th.bin"):
-			print "bl1 image name: g-bios-th.bin"
-			bl1_page_offset	= th_part_size
-			put_no_oob("g-bios-th.bin", bl1_page_offset, flash_page_size)
-			print "put bl1 into flash image done!"
-			break
+	print "add " + bl1  + " into image"
+	bl1_page_offset	= th_part_size
+	mk.put_no_oob(bl1, bl1_page_offset)
+	print "put " + bl1 + " into flash image done!"
 
-		if case("g-bios-bh.bin"):
-			print "bl2 image name: g-bios-th.bin"
-			bl2_page_offset = bh_part_size / flash_page_size
-			put_no_oob("g-bios-bh.bin", bl2_page_offset, flash_page_size)
-			print "put BL2 into flash image done!"
-			break
+	print "add " + bl2  + " into image"
+	bl2_page_offset = bh_part_size / mk.flash_page_size
+	mk.put_no_oob(bl2, bl2_page_offset)
+	print "put " + bl2 + " into flash image done!"
 
-		if case("zImage"):
-			print "Linux kernel image name: zImage"
-			kernel_page_offset = ker_part_size / flash_page_size
-			put_no_oob("zImage", kernel_page_offset, flash_page_size)
-			print "put Linux kernel into flash image done!"
-			break
-
-		if case("jffs2"):
-			print "rootfs image name: jffs2"
-			jffs2_page_offset = jf2_part_size / flash_page_size
-			put_no_oob("jffs2", jffs2_page_offset, flash_page_size)
-			print "put jffs2 into flash image done!"
-			break
-
-		if case("yaffs2"):
-			print "rootfs image name: yaffs2"
-			yaffs2_page_offset = yaf_part_size / flash_page_size
-			put_no_oob("yaffs2", yaffs2_page_offset, flash_page_size)
-			print "put yaffs2 into flash image done!"
-			break
-
-		if case("ubifs"):
-			print "rootfs image name: ubifs"
-			ubifs_page_offset = ubi_part_size / flash_page_size
-			put_no_oob("ubifs", ubifs_page_offset, flash_page_size)
-			print "put ubifs into flash image done!"
-			break
-		else:
-			usage()
-			sys.exit()
+	print "add " + bl3  + " into image"
+	bl3_page_offset = cfg_part_size / mk.flash_page_size
+	mk.put_no_oob(bl3, bl3_page_offset)
+	print "put " + bl3 + " into flash image done!"
