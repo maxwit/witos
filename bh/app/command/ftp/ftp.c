94c01860a5569580fb906a4fc03cbf391c9785c2
Hank Pan <hank.pan@maxwit.com>
2011-10-25 23:55:13 +0800

    FTP: improve and add functons
    Signed-off-by: Hank Pan <hank.pan@maxwit.com>
    Signed-0ff-by: Jason Zhou <jason.zhou@maxwit.com>

--- bh/app/command/ftp/ftp.c
+++ bh/app/command/ftp/ftp.c
@@ -4,22 +4,36 @@
 #include <errno.h>
 #include <net/net.h>
 #include <fs/fs.h>
+#include <sysconf.h>
+#include <shell.h>
 
 #define PATH "/srv/ftp"
 #define BUF_LEN 512
 #define FTP_PORT 21
-#define O_CREAT 100
-#define O_RDWR  2
+#define USER_PSWD_ERROR "530"
+#define NO_SUCH_FILE    "550"
 
-//fix me
-int port_atoi(const char *str)
+struct ftp_opt
+{
+	__u16 svr_port;
+	__u32 svr_ip;
+	char src_file[BUF_LEN];
+	char dst_file[BUF_LEN];
+	char user[BUF_LEN];
+	char pswd[BUF_LEN];
+	char type[BUF_LEN];
+	char mem[BUF_LEN];
+	bool def_user_flag;
+	bool mem_flag;
+};
+
+static int port_atoi(const char *str)
 {
 	const char *iter;
 	int interger = 0;
 	int temp;
 
-	for (iter = str; *iter >= '0' && *iter <= '9'; iter++)
-	{
+	for (iter = str; *iter >= '0' && *iter <= '9'; iter++) {
 		temp = *iter - '0';
 		interger = interger * 10 + temp;
 	}
@@ -27,15 +41,13 @@ int port_atoi(const char *str)
 	return interger;
 }
 
-static int connect_port(unsigned short port, const char *ip)
+static int connect_port(const struct ftp_opt *fopt)
 {
 	int sockfd, ret;
 	struct sockaddr_in server_addr, local_addr;
-	__u32 svr_ip;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-	{
+	if (sockfd < 0) {
 		return sockfd;
 	}
 
@@ -44,22 +56,19 @@ static int connect_port(unsigned short port, const char *ip)
 	local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	ret = bind(sockfd, (struct sockaddr *)&local_addr, sizeof(local_addr));
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printf("bind error\n");
 		sk_close(sockfd);
 		return ret;
 	}
-	str_to_ip((__u8 *)&svr_ip, ip);
 
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
-	server_addr.sin_addr.s_addr = svr_ip;
+	server_addr.sin_port = htons(fopt->svr_port);
+	server_addr.sin_addr.s_addr = fopt->svr_ip;
 
 	ret = connect(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printf("connect error\n");
 		sk_close(sockfd);
 
@@ -69,29 +78,26 @@ static int connect_port(unsigned short port, const char *ip)
 	return sockfd;
 }
 
-static unsigned short get_data_port(int sockfd)
+static int get_data_port(int sockfd)
 {
-	unsigned short data_port;
+	int data_port;
 	int ret;
 	char buff[BUF_LEN];
 	char *p;
 
 	ret = send(sockfd, "PASV\r\n", 6, 0);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printf("send error\n");
 		return ret;
 	}
 
 	ret = recv(sockfd, buff, BUF_LEN - 1, 0);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printf("recv error\n");
 		return ret;
 	}
 
 	buff[ret] = '\0';
-
 	printf("%s\n", buff);
 
 	p = strrchr(buff, ',');
@@ -101,160 +107,321 @@ static unsigned short get_data_port(int sockfd)
 	p = strrchr(buff, ',');
 	data_port += port_atoi(p + 1) << 8;
 
-	printf("server deliver data port: %d\n", data_port);
 	return data_port;
 }
 
 static int ftp_send_cmd(int cmd_fd, const char *cmd, const char *arg)
 {
-	int i, j;
 	char buff[BUF_LEN];
 	int ret;
 
-	for (i = 0; cmd[i]; i++)
-	{
-		buff[i] = cmd[i];
-	}
-
 	if (arg != NULL)
-	{
-		buff[i++] = ' ';
-		for (j = 0; arg[j]; j++)
-		{
-			buff[i++] = arg[j];
-		}
-	}
-
-	buff[i++] = '\r';
-	buff[i++] = '\n';
-	buff[i]   = '\0';
+		snprintf(buff, BUF_LEN, "%s %s\r\n", cmd, arg);
+	else
+		sprintf(buff, "%s\r\n", cmd);
 
 	ret = send(cmd_fd, buff, strlen(buff), 0);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printf("send error\n");
 		return ret;
 	}
 
 	ret = recv(cmd_fd, buff, BUF_LEN - 1, 0);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		printf("recv error\n");
 		return ret;
 	}
-	if (!strncmp(buff, "530", 3))
-	{
-		ret = -1;
-		printf("User or Password error\n");
-	}
-	else if (!strncmp(buff, "550", 3))
-	{
-		ret = -1;
-		printf("no such file\n");
-	}
 
 	buff[ret] = '\0';
+
+	if (!strncmp(buff, USER_PSWD_ERROR, 3)) {
+		ret = -1;
+	}
+	else if (!strncmp(buff, NO_SUCH_FILE, 3)) {
+		ret = -1;
+	}
 
 	printf("%s\n", buff);
 
 	return ret;
 }
 
-static int ftp_download_file(int cmd_fd, const char *ip, const char *filename)
+//download to flash
+static int get_file_to_flash(int data_fd)
 {
+	int ret;
+	char buff[BUF_LEN];
+	char cur_vol;
+	struct block_device *bdev;
+	struct bdev_file *file;
+
+	cur_vol = get_curr_volume();
+
+	bdev = get_bdev_by_volume(cur_vol);
+	if (NULL== bdev)
+	{
+		printf("bdev null\n");
+		return -1;
+	}
+
+	file = bdev->file;
+	if (NULL == file)
+	{
+		printf("file null\n");
+		return 0;
+	}
+
+	file->bdev = file;
+
+	file->open(file,"jffs2");
+
+	while (1) {
+		ret = recv(data_fd, buff, BUF_LEN, 0);
+		if (ret < 0) {
+			printf("recv error\n");
+			break;
+		}
+
+		file->write(file, buff, ret);
+		if (ret < BUF_LEN) {
+			break;
+		}
+	}
+
+	return 0;
+}
+
+static int get_file_to_mem(int data_fd, const struct ftp_opt *fopt)
+{
+	__u32 addr;
+	char *p;
+	int ret;
+	int i;
+	char buff[BUF_LEN];
+
+	str_to_val(fopt->mem, &addr);
+	p = addr;
+
+	while(1) {
+		ret = recv(data_fd, buff, BUF_LEN - 1 , 0);
+		if (ret < 0) {
+			printf("recv error\n");
+			break;
+		}
+
+		buff[ret] = '\0';
+
+		//write
+		for (i = 0; buff[i]; i++) {
+			*p++ = buff[i];
+			printf("%c", *(p - 1));
+		}
+
+		if (ret < BUF_LEN - 1)
+			break;
+	}
+
+	printf("\n");
+
+#if 0
+	*p = '\0';
+	p = addr;
+	printf("%x\n", p);
+	printf("%s\n", p);
+#endif
+
+	return 0;
+}
+
+static int ftp_download_file(int cmd_fd, struct ftp_opt *fopt)
+{
+	int data_port;
 	int data_fd;
 	int ret = 0;
-	unsigned short data_port;
-	char buff[BUF_LEN];
 	char path[BUF_LEN];
 
-	sprintf(path, PATH"/%s", filename);
-	printf("%s\n", path);
+	if (!fopt->def_user_flag)
+		sprintf(path, PATH"/%s", fopt->src_file);
+	else
+		sprintf(path, "%s", fopt->src_file);
 
 	data_port = get_data_port(cmd_fd);
-	if (data_port < 0)
-	{
+	if (data_port < 0) {
 		printf("server deliver port error\n");
 		return data_port;
 	}
 
-	data_fd = connect_port(data_port, ip);
-	if (data_fd < 0)
-	{
+	fopt->svr_port = data_port;
+	data_fd = connect_port(fopt);
+	if (data_fd < 0) {
 		return data_fd;
 	}
 
 	ret = ftp_send_cmd(cmd_fd, "RETR", path);
-	if (ret < 0)
-	{
-		printf("get file error\n");
+	if (ret < 0) {
 		return ret;
 	}
-#if 0
-	fd = open(filename, O_CREAT | O_RDWR, 0666);
-	if (fd < 0)
-	{
-		printf("open error\n");
-		return fd;
-	}
-#endif
 
-	while (1)
-	{
-		ret = recv(data_fd, buff, BUF_LEN - 1, 0);
-		if (ret < 0)
-		{
-			printf("recv error\n");
-			break;
-		}
-#if 0
-		write(fd, buff, ret);
-#endif
-		buff[ret] = '\0';
-		printf("%s\n", buff);
-
-		if (ret < BUF_LEN)
-		{
-			break;
-		}
-	}
-#if 0
-	sk_close(fd);
-#endif
+	if (!fopt->mem_flag)
+		get_file_to_flash(data_fd);
+	else
+		get_file_to_mem(data_fd, fopt);
 
 	sk_close(data_fd);
+
 	return ret;
 }
 
-int ftp_login(int cmd_fd, char *user, char *pswd) //fix me
+static int ftp_upload_file(int cmd_fd,  struct ftp_opt *fopt)
 {
-	int ret;
+	int data_port;
+	int data_fd;
+	int ret = 0;
+	char buff[BUF_LEN] = "It's just for test!";
+	char path[BUF_LEN];
 
-	ret = ftp_send_cmd(cmd_fd, "USER", user);
-	if (ret < 0)
-	{
+	if (!fopt->def_user_flag)
+		sprintf(path, PATH"/%s", fopt->src_file);
+	else
+		sprintf(path, "%s", fopt->src_file);
+
+	data_port = get_data_port(cmd_fd);
+	if (data_port < 0) {
+		printf("server deliver port error\n");
+		return data_port;
+	}
+
+	fopt->svr_port = data_port;
+
+	data_fd = connect_port(fopt);
+	if (data_fd < 0) {
+		return data_fd;
+	}
+
+	ret = ftp_send_cmd(cmd_fd, "STOR", path);
+	if (ret < 0) {
+		printf("put file error\n");
 		return ret;
 	}
 
-	ret = ftp_send_cmd(cmd_fd, "PASS", pswd);
-	if (ret < 0)
-	{
+	ret = send(data_fd, buff, strlen(buff), 0);
+	if (ret < 0) {
+		printf("send error\n");
+		return ret;
+	}
+
+	sk_close(data_fd);
+
+	return ret;
+}
+
+static int ftp_login(int cmd_fd, const struct ftp_opt *fopt) //fix me
+{
+	int ret;
+
+	ret = ftp_send_cmd(cmd_fd, "USER", fopt->user);
+	if (ret < 0) {
+		return ret;
+	}
+
+	ret = ftp_send_cmd(cmd_fd, "PASS", fopt->pswd);
+	if (ret < 0) {
 		return ret;
 	}
 
 	ret = ftp_send_cmd(cmd_fd, "SYST", NULL);
-	if (ret < 0)
-	{
+	if (ret < 0) {
 		return ret;
 	}
 
-	ret = ftp_send_cmd(cmd_fd, "TYPE", "I");
-	if (ret < 0)
-	{
+	ret = ftp_send_cmd(cmd_fd, "TYPE", fopt->type);
+	if (ret < 0) {
 		return ret;
 	}
 
 	return 0;
+}
+
+static int get_arg(char *arg, struct ftp_opt *fopt)
+{
+	int i ;
+	int port = 0;
+	char *cp;
+
+	//if specialize user(not anonymous)
+	if (*arg != '@')
+	{
+		if ((arg = strchr(arg, ':')) != NULL)
+			arg++;
+
+		for (i = 0; *arg; arg++) {
+			if (*arg == ':' || *arg == '@') {
+				if (*arg == '@') {
+					printf("Please specific the password\n");
+					return -1;
+				}
+				fopt->user[i] = '\0';
+				fopt->def_user_flag = false;
+				break;
+			}
+
+			fopt->user[i++] = *arg;
+		}
+
+		for (i = 0, arg++; *arg ; arg++) {
+			if (*arg == ':' || *arg == '@' ) {
+				fopt->pswd[i] = '\0';
+				break;
+			}
+
+			fopt->pswd[i++] = *arg;
+		}
+	}
+
+	arg++;
+	//if specialize port number
+	if ((cp = strchr(arg, ':')) != NULL) {
+		*cp = '\0';
+		str_to_ip((__u8 *)&fopt->svr_ip, arg);
+		arg = cp + 1;
+
+		port = port_atoi(arg);
+		if (port == FTP_PORT)
+			fopt->svr_port = port;
+		else  {
+			printf("invalid port for FTP\n");
+			return -1;
+		}
+		//if specialize filename
+		if ((cp = strchr(arg, '/')) != NULL) {
+			*cp = '\0';
+			arg = cp + 1;
+			if (*arg)
+				memcpy(fopt->src_file, arg, BUF_LEN);
+
+			*arg = '\0';
+		}
+	}
+	//if lack of port number, and specialize filename
+	else if ((cp = strchr(arg, '/'))!= NULL) {
+		*cp = '\0';
+		str_to_ip((__u8 *)&fopt->svr_ip, arg);
+		arg = cp + 1;
+		if (*arg)
+			memcpy(fopt->src_file, arg, BUF_LEN);
+
+		*arg = '\0';
+	}
+
+	//if only ip behind '@'
+	if (*arg)
+		str_to_ip((__u8 *)&fopt->svr_ip, arg);
+
+	printf("user = %s, pass = %s, filename = %s \n", fopt->user,
+			fopt->pswd, fopt->src_file);
+
+	return 0;
+
 }
 
 int main(int argc, char *argv[])
@@ -262,88 +429,99 @@ int main(int argc, char *argv[])
 	int cmd_fd;
 	int ret = 0;
 	int ch;
-	int count = 0;
-	char user[BUF_LEN];
-	char pswd[BUF_LEN];
-	char svr_ip[BUF_LEN];
 	char filename[BUF_LEN];
 	char buff[BUF_LEN];
+	bool get_put_flag = true;
+	struct ftp_opt fopt;
 
-	while((ch = getopt(argc, argv, "u:p:s:f:h")) != -1)
-	{
-		switch(ch)
-		{
-		case 'u':
-			strncpy(user, optarg, BUF_LEN);
-			count++;
-			break;
+	memset(filename, 0, sizeof(filename));
+	memset(&fopt, 0, sizeof(fopt));
 
-		case 'p':
-			strncpy(pswd, optarg, BUF_LEN);
-			count++;
-			break;
+	strncpy(fopt.user, "anonymous", BUF_LEN);
+	strncpy(fopt.pswd, "any", BUF_LEN);
 
-		case 's':
-			strncpy(svr_ip, optarg, BUF_LEN);
-			count++;
-			break;
+	net_get_server_ip(&fopt.svr_ip);
+	strncpy(fopt.src_file, "a", BUF_LEN);
+	strncpy(fopt.type, "I", BUF_LEN);
 
+	fopt.svr_port = FTP_PORT;
+	fopt.def_user_flag = true;
+	fopt.mem_flag = false;
+
+	if (strncmp(argv[1], "put", 3) == 0) {
+		get_put_flag = false;
+	}
+
+	ret = argc - 1;
+
+	if (argc >= 2 && (!strncmp(argv[ret], "user", 4)  || *(argv[ret]) == '@') )
+		ret = get_arg(argv[ret], &fopt);
+		if (ret < 0)
+			return ret;
+
+	while((ch = getopt(argc, argv, "f:m:v:h")) != -1) {
+
+		switch(ch) {
 		case 'f':
 			strncpy(filename, optarg, BUF_LEN);
-			count++;
+			break;
+
+		case 'm':
+			fopt.mem_flag = true;
+			strncpy(fopt.mem, optarg, BUF_LEN);
+			break;
+
+		case 'v':
+			strncpy(fopt.type, optarg, BUF_LEN);
 			break;
 
 		case 'h':
 		default:
-			printf("Usage: -u[...] -p[...] -s[...] -f[...]\n"
-				   "-u username\n"
-				   "-p password\n"
-				   "-s server ip\n"
-				   "-f file name\n"
-		                "-h help\n       ");
+			usage();
 			return -EINVAL;
-
 		}
 	}
 
-	if (count != 4)
-	{
-	      printf("Usage: -u[...] -p[...] -s[...] -f[...]\n"
-			  "-u username\n"
-			  "-p password\n"
-			  "-s server ip\n"
-			  "-f file name\n"
-			  "-h help\n       ");
-		return -EINVAL;
+	if (get_put_flag && filename[0])
+		memcpy(fopt.dst_file, filename, BUF_LEN);
+
+	else if(!get_put_flag && filename[0]) {
+		memcpy(fopt.src_file, filename, BUF_LEN);
 	}
-	//control connection
-	cmd_fd = connect_port(FTP_PORT, svr_ip);
-	if (cmd_fd < 0)
-	{
+
+	// control connection
+	cmd_fd = connect_port(&fopt);
+	if (cmd_fd < 0) {
 		return cmd_fd;
 	}
 
-	ret = recv(cmd_fd, buff, BUF_LEN, 0);//need to wait for reply, or causes problem;
-	if (ret < 0)
-	{
+	ret = recv(cmd_fd, buff, BUF_LEN, 0);// need to wait for reply, or causes problem;
+	if (ret < 0) {
 		printf("recv error\n");
 		goto L1;
 	}
 
-	//login
-	ret = ftp_login(cmd_fd, user, pswd);
-	if (ret < 0)
-	{
+	// login
+	ret = ftp_login(cmd_fd, &fopt);
+	if (ret < 0) {
 		goto L1;
 	}
 
-	//download
-	ret = ftp_download_file(cmd_fd, svr_ip, filename);
-	if (ret < 0)
-	{
-		printf("download failed\n");
+	// download
+	if (get_put_flag) {
+		ret = ftp_download_file(cmd_fd, &fopt);
+		if (ret < 0) {
+			printf("download failed\n");
+		}
+	}else {//upload
+		ret = ftp_upload_file(cmd_fd, &fopt);
+		if (ret < 0) {
+			printf("upload failed\n");
+		}
 	}
+
 L1:
 	sk_close(cmd_fd);
+
 	return ret;
 }
