6602ffd067236e8658376996e263899bbbbcf01a
Matthew He <matthew.he@maxwit.com>
2011-10-25 21:20:17 +0800

    tftp: fix tftp command resolution and add tftp put file function
    	1.add two subcommand 'get' and 'put'.
    	2.fix command option resolution.
    	3.add new function tftp_put_file()、tftp_upload()
    
    Signed-off-by: Matthew He <matthew.he@maxwit.com>
    Signed-off-by: Yucheng Mo <yucheng.mo@maxwit.com>

--- bh/lib/net/tftp.c
+++ bh/lib/net/tftp.c
@@ -13,7 +13,12 @@ struct tftp_packet
 	__u8 data[0];
 } __PACKED__;
 
-static int tftp_make_rrq(__u8 *buff, const char *file_name)
+struct str_part_type {
+	char *str;
+	PART_TYPE type;
+};
+
+static int tftp_make_rrq(__u8 *buff, const char *file_name, const char *mode)
 {
 	int len;
 
@@ -26,11 +31,49 @@ static int tftp_make_rrq(__u8 *buff, const char *file_name)
 	buff[len] = '\0';
 	len += 1;
 
-	strcpy((char *)buff + len, TFTP_MODE_OCTET);
-	len += strlen(TFTP_MODE_OCTET);
+	strcpy((char *)buff + len, mode);
+	len += strlen(mode);
 
 	buff[len] = '\0';
 	len += 1;
+
+	return len;
+}
+
+static int tftp_make_wrq(__u8 *buff, const char *file_name, const char *mode)
+{
+	int len;
+
+	*(__u16 *)buff = TFTP_WRQ;
+	len = 2;
+
+	strcpy((char *)buff + len, file_name);
+	len += strlen(file_name);
+
+	buff[len] = '\0';
+	len += 1;
+
+	strcpy((char *)buff + len, mode);
+	len += strlen(mode);
+
+	buff[len] = '\0';
+	len += 1;
+
+	return len;
+}
+
+static int tftp_make_data(void *buff, __u16 blk_num, const void *data, __u16 pkt_len)
+{
+	int len;
+
+	*(__u16 *)buff = TFTP_DAT;
+	len = 2;
+
+	*(__u16 *)(buff + len) = htons(blk_num);
+	len += 2;
+
+	memcpy((char *)buff + len, data, pkt_len);
+	len += pkt_len;
 
 	return len;
 }
@@ -51,27 +94,25 @@ int tftp_download(struct tftp_opt *opt)
 {
 	int  ret;
 	int  sockfd;
-	__u8   buf[TFTP_BUF_LEN];
-	char server_ip[IPV4_STR_LEN], local_ip[IPV4_STR_LEN];
-	__u32  pkt_len, load_len;
 	__u16  blk_num;
 	__u8  *buff_ptr;
 	__u32  client_ip;
+	socklen_t addrlen;
+	__u8   buf[TFTP_BUF_LEN];
+	__u32  pkt_len, load_len;
+	struct bdev_file *file;
 	struct tftp_packet *tftp_pkt;
 	struct sockaddr_in *local_addr, *remote_addr;
-	struct bdev_file *file;
-	socklen_t addrlen;
+	char server_ip[IPV4_STR_LEN], local_ip[IPV4_STR_LEN];
 
 	ndev_ioctl(NULL, NIOC_GET_IP, &client_ip);
 
-	if (ip_to_str(local_ip, client_ip) < 0)
-	{
+	if (ip_to_str(local_ip, client_ip) < 0) {
 		printf("Error: Local IP!\n");
 		return -EINVAL;
 	}
 
-	if (ip_to_str(server_ip, opt->server_ip) < 0)
-	{
+	if (ip_to_str(server_ip, opt->server_ip) < 0) {
 		printf("Error: Server IP!\n");
 		return -EINVAL;
 	}
@@ -80,15 +121,13 @@ int tftp_download(struct tftp_opt *opt)
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	if (sockfd <= 0)
-	{
+	if (sockfd <= 0) {
 		printf("%s(): error @ line %d!\n", __func__, __LINE__);
 		return -EIO;
 	}
 
 	local_addr = malloc(sizeof(*local_addr));
-	if (local_addr == NULL)
-	{
+	if (local_addr == NULL) {
 		printf("%s(): error @ line %d!\n", __func__, __LINE__);
 		return -ENOMEM;
 	}
@@ -100,11 +139,14 @@ int tftp_download(struct tftp_opt *opt)
 
 	printf(" \"%s\": %s => %s\n", opt->file_name, server_ip, local_ip);
 
-	pkt_len = tftp_make_rrq((__u8 *)tftp_pkt, opt->file_name);
+	if (!opt->mode[0]) {
+		pkt_len = tftp_make_rrq((__u8 *)tftp_pkt, opt->file_name, TFTP_MODE_OCTET);
+	} else {
+		pkt_len = tftp_make_rrq((__u8 *)tftp_pkt, opt->file_name, opt->mode);
+	}
 
 	remote_addr = malloc(sizeof(*remote_addr));
-	if (remote_addr == NULL)
-	{
+	if (remote_addr == NULL) {
 		printf("%s(): error @ line %d!\n", __func__, __LINE__);
 		return -ENOMEM;
 	}
@@ -121,9 +163,6 @@ int tftp_download(struct tftp_opt *opt)
 	load_len = 0;
 	blk_num  = 1;
 
-	// fixme!!!
-	opt->type = "jffs2";
-
 	if (file) {
 		ret = file->open(file, opt->type);
 		if (ret < 0) {
@@ -132,8 +171,7 @@ int tftp_download(struct tftp_opt *opt)
 		}
 	}
 
-	do
-	{
+	do {
 		pkt_len = recvfrom(sockfd, tftp_pkt, TFTP_BUF_LEN, 0,
 						(struct sockaddr *)remote_addr, &addrlen);
 		if(0 == pkt_len)
@@ -144,19 +182,16 @@ int tftp_download(struct tftp_opt *opt)
 		if (pkt_len > TFTP_PKT_LEN)  // fixme
 			pkt_len = TFTP_PKT_LEN;
 
-		switch (tftp_pkt->op_code)
-		{
+		switch (tftp_pkt->op_code) {
 		case TFTP_DAT:
-			if (ntohs(tftp_pkt->block) == blk_num)
-			{
+			if (ntohs(tftp_pkt->block) == blk_num) {
 				tftp_send_ack(sockfd, blk_num, remote_addr);
 				blk_num++;
 
 				load_len += pkt_len;
 
 #ifdef TFTP_VERBOSE
-				if ((load_len & 0x3fff) == 0 || TFTP_PKT_LEN != pkt_len)
-				{
+				if ((load_len & 0x3fff) == 0 || TFTP_PKT_LEN != pkt_len) {
 					char tmp[32];
 
 					val_to_hr_str(load_len, tmp);
@@ -164,8 +199,7 @@ int tftp_download(struct tftp_opt *opt)
 				}
 #endif
 
-				if (NULL != buff_ptr)
-				{
+				if (NULL != buff_ptr) {
 					memcpy(buff_ptr, tftp_pkt->data, pkt_len);
 					buff_ptr += pkt_len;
 				}
@@ -175,9 +209,7 @@ int tftp_download(struct tftp_opt *opt)
 					if (ret < 0)
 						goto L1;
 				}
-			}
-			else
-			{
+			} else {
 #ifdef TFTP_DEBUG
 				printf("\t%s(): LOST Packet = 0x%x (0x%x).\r",
 					__func__, blk_num, ntohs(tftp_pkt->block));
@@ -217,3 +249,200 @@ L1:
 	return load_len;
 }
 
+int tftp_upload(struct tftp_opt *opt)
+{
+	int  ret;
+	int  sockfd;
+	__u16  blk_num;
+	char  *buff_ptr;
+	__u32  client_ip;
+	socklen_t addrlen;
+	__u8   buf[TFTP_BUF_LEN];
+	__u32  pkt_len, send_len, dat_len;
+	struct tftp_packet *tftp_pkt;
+	struct sockaddr_in *local_addr, *remote_addr;
+	char server_ip[IPV4_STR_LEN], local_ip[IPV4_STR_LEN];
+
+	ndev_ioctl(NULL, NIOC_GET_IP, &client_ip);
+
+	if (ip_to_str(local_ip, client_ip) < 0) {
+		printf("Error: Local IP!\n");
+		return -EINVAL;
+	}
+
+	if (ip_to_str(server_ip, opt->server_ip) < 0) {
+		printf("Error: Server IP!\n");
+		return -EINVAL;
+	}
+
+	tftp_pkt = (struct tftp_packet *)buf;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (sockfd <= 0) {
+		printf("%s(): error @ line %d!\n", __func__, __LINE__);
+		return -EIO;
+	}
+
+	local_addr = malloc(sizeof(*local_addr));
+	if (local_addr == NULL) {
+		printf("%s(): error @ line %d!\n", __func__, __LINE__);
+		return -ENOMEM;
+	}
+
+	memset(local_addr, 0, sizeof(*local_addr));
+	// local_addr->sin_port = port_alloc(SOCK_DGRAM);
+	local_addr->sin_addr.s_addr = client_ip;
+	ret = bind(sockfd, (struct sockaddr *)local_addr, sizeof(struct sockaddr));
+
+	printf(" \"%s\": %s => %s\n", opt->file_name, local_ip, server_ip);
+
+	if (!opt->mode[0]) {
+		pkt_len = tftp_make_wrq((__u8 *)tftp_pkt, opt->file_name, TFTP_MODE_OCTET);
+	} else {
+		pkt_len = tftp_make_wrq((__u8 *)tftp_pkt, opt->file_name, opt->mode);
+	}
+
+	remote_addr = malloc(sizeof(*remote_addr));
+	if (remote_addr == NULL) {
+		printf("%s(): error @ line %d!\n", __func__, __LINE__);
+		return -ENOMEM;
+	}
+
+	memset(remote_addr, 0, sizeof(*remote_addr));
+	remote_addr->sin_addr.s_addr = opt->server_ip; // bigendian
+	remote_addr->sin_port = htons(STD_PORT_TFTP);
+
+	sendto(sockfd, tftp_pkt, pkt_len, 0,
+		(struct sockaddr *)remote_addr, sizeof(*remote_addr));
+#if 0
+	file     = opt->file;
+#endif
+
+	buff_ptr = opt->load_addr;
+	send_len = 0;
+	blk_num  = 0;
+	int file_size = 10230; //fixme!! ?
+
+#if 0
+	if (file) {
+		ret = file->open(file, opt->type);
+		if (ret < 0) {
+			printf("fail to open \"%s\"!\n", file->bdev->dev.name);
+			goto L1;
+		}
+	}
+#endif
+
+	do {
+		pkt_len = recvfrom(sockfd, tftp_pkt, TFTP_BUF_LEN, 0,
+						(struct sockaddr *)remote_addr, &addrlen);
+		if(0 == pkt_len)
+			goto L1;
+
+		switch (tftp_pkt->op_code) {
+		case TFTP_ACK:
+			if (ntohs(tftp_pkt->block) == blk_num) {
+				dat_len = file_size > TFTP_PKT_LEN ? TFTP_PKT_LEN : file_size;
+#if 0
+				ret = file->read(file, tftp_pkt->data, dat_len);
+				blk_num++;
+
+				tftp_pkt->op_code = TFTP_DAT;
+				tftp_pkt->block= htons(blk_num);
+
+				pkt_len += TFTP_HDR_LEN;
+#endif
+				if (NULL != buff_ptr) {
+					pkt_len = tftp_make_data((__u8 *)tftp_pkt, ++blk_num, buff_ptr, dat_len);
+				}
+				sendto(sockfd, tftp_pkt, pkt_len, 0,
+						(struct sockaddr *)remote_addr, sizeof(*remote_addr));
+
+				send_len	+= dat_len;
+				file_size	-= dat_len;
+				buff_ptr	+= dat_len;
+
+#ifdef TFTP_VERBOSE
+				if ((send_len & 0x3fff) == 0 || TFTP_PKT_LEN != pkt_len) {
+					char tmp[32];
+
+					val_to_hr_str(send_len, tmp);
+					printf("\r %d(%s) sended  ", send_len, tmp);
+				}
+#endif
+			} else {
+#ifdef TFTP_DEBUG
+				printf("\t%s(): LOST Packet = 0x%x (0x%x).\r",
+					__func__, blk_num, ntohs(tftp_pkt->block));
+#endif
+				pkt_len = recvfrom(sockfd, tftp_pkt, TFTP_BUF_LEN, 0,
+									(struct sockaddr *)remote_addr, &addrlen);
+			}
+
+			break;
+
+		case TFTP_ERR:
+			printf("\n%s(): %s (Error num = %d)\n",
+				__func__, tftp_pkt->data, ntohs(tftp_pkt->error));
+
+			ret = -EIO;
+			goto L1;
+
+		default:
+			printf("\n%s(): Unsupported opcode 0x%02x! (CurBlkNum = %d)\n",
+				__func__, ntohs(tftp_pkt->op_code), blk_num);
+
+			ret = -EIO;
+			goto L1;
+		}
+	} while (file_size > 0);
+
+L1:
+#ifdef TFTP_VERBOSE
+	printf("\n");
+#endif
+#if 0
+	if (file)
+		file->close(file);
+#endif
+	sk_close(sockfd);
+	free(remote_addr);
+	free(local_addr);
+
+	return send_len;
+}
+
+PART_TYPE str2part_type(const char *str)
+{
+	int i;
+	struct str_part_type type[] = {
+		{
+			.str	= "g-bios-th",
+			.type	= PT_BL_GTH,
+		},
+		{
+			.str	= "g-bios-bh",
+			.type	= PT_BL_GBH,
+		},
+		{
+			.str	= "sys_conf",
+			.type	= PT_BL_GCONF,
+		},
+		{
+			.str	= "linux",
+			.type	= PT_OS_LINUX,
+		},
+		{
+			.str	= "uboot",
+			.type	= PT_BL_UBOOT,
+		},
+	};
+
+	for (i = 0; i < ARRAY_ELEM_NUM(type); i++) {
+		if (0 == strcmp(type[i].str, str)) {
+			return type[i].type;
+		}
+	}
+	return PT_NONE;
+}
