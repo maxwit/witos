2316df6b8cc34db6f16fe2cf3acd6b11dceeb8ad
Kaka Li <kaka.li@maxwit.com>
2011-10-27 17:57:26 +0800

    fix config
    Signed-off-by: Kaka Li <kaka.li@maxwit.com>
    Signed-off-by: Peng Xiao<plove.xiao@maxwit.com>

--- bh/app/command/basic/config.c
+++ bh/app/command/basic/config.c
@@ -2,49 +2,112 @@
 #include <sysconf.h>
 #include <getopt.h>
 
+#define LEN 128
+
+static int exec_conf(int option, char *arg, char *val)
+{
+	char buff[LEN];
+	int ret;
+
+	switch (option) {
+	case 'a':
+		ret = conf_add_attr(arg, val);
+		if (ret >= 0)
+			printf("add %s successfully\n", arg);
+
+		break;
+
+	case 'd':
+		ret = conf_del_attr(arg);
+		if (ret >= 0)
+			printf("delete %s successfully\n", arg);
+
+		break;
+
+	case 'g':
+		ret = conf_get_attr(arg, buff);
+		if (!ret)
+			printf("%s = %s\n", arg, buff);
+
+		break;
+
+	case 'l':
+		ret = conf_list_attr();
+
+		break;
+
+	case 's':
+		ret = conf_set_attr(arg, val);
+		if (ret >= 0)
+			printf("set %s successfully\n", arg);
+
+		break;
+	}
+
+	return ret;
+}
+
+
 int main(int argc, char *argv[])
 {
-	int ret, opt;
-	bool reset_all = false;
+	int opt, option = 0;
+	int ret = 0;
+	char *val = NULL;
+	char *arg = NULL;
 
-	usage();
+	if (argc == 1) {
+		usage();
+		return -EINVAL;
+	}
 
-	while ((opt = getopt(argc, argv, "r:h")) != -1)
+	while ((opt = getopt(argc, argv, "a:g:d:s:hl")) != -1)
 	{
-		switch (opt)
-		{
-		case 'r':
-			if (!strcmp(optarg, "all"))
-			{
-				reset_all = true;
-				printf("reseting all configuration to default!");
-			}
-			else if (!strcmp(optarg, "net"))
-			{
-				printf("reseting network configuration to default!");
-			}
-			else if (!strcmp(optarg, "boot"))
-			{
-				printf("reseting Linux configuration to default!");
-			}
-			else
-			{
+		switch (opt) {
+		case 'l':
+		case 'd':
+		case 'g':
+			if (optind < argc) {
 				usage();
 				return -EINVAL;
 			}
+
 			break;
 
-		case 'l':
-			conf_list_attr();
+		case 'a':
+		case 's':
+			if (optind < argc - 1) {
+				usage();
+				return -EINVAL;
+			}
+
 			break;
+
+		case 'h':
+			usage();
+			return ret;
 
 		default:
 			usage();
 			return -EINVAL;
 		}
+
+		option = opt;
+		arg = optarg;
 	}
 
-	ret = conf_store();
+	if (!option)
+		return -EINVAL;
+
+	val = argv[optind];
+	ret = exec_conf(option, arg, val);
+	if (ret < 0)
+		return -EINVAL;
+
+	if (option != 'l' && option != 'g' && option != 'h') {
+		ret = conf_store();
+		if (ret)
+			printf("\nconfig_store faild\n");
+	}
 
 	return ret;
 }
